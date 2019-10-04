e1c18e98856fa7cc71bc6061041ba2c6b976196a
RashadTanjim <rashad.tanjim@northsouth.edu>
2019-10-05 02:38:45 +0600

    navBar, splash screen, app row data, custom font, color, style decleared, design related dependancy added, menifesto updated, icon added

---
+++ Android App - Inception/app/src/main/java/com/mastert/inception/Splash_screen.java
@@ -0,0 +1,47 @@
+package com.mastert.inception;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 3 seconds
+                    sleep(3*1000);
+
+                    Intent intent = new Intent(Splash_screen.this,navBar.class);
+                    startActivity(intent);
+
+                    // After 3 seconds redirect to another intent
+
+                    //Remove activity
+                    finish();
+
+                } catch (Exception e) {
+
+                }
+            }
+        };
+
+        // start thread
+        background.start();
+
+
+    }
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+
+    }
+}
